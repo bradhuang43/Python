2c0b3fe229db0d0d546df0951e566e864b533b80
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-04-17 19:32:08 -0700

    Create Max.py

---
+++ Max.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'max of {a} and {b} is {max(a, b)}')
