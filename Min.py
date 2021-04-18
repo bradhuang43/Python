acee3890dce226f409c904fed7d75a503126ce79
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-04-17 19:36:11 -0700

    Create Min.py

---
+++ Min.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def min(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+
+a = (input('Enter 1st number: '))
+b = (input('Enter 2nd number: '))
+
+print(f'min of {a} and {b} is {min(a, b)}')
