f96343c8f5170328d1c2db1fc21be75de31f1a2d
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-04-21 18:49:01 -0700

    Create tiger.py

---
+++ tiger.py
@@ -0,0 +1,16 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def readyfortiger(a):
+    if a <= 0:
+        return ('you got to be kiding')
+    elif a< 100:
+        return ('tiger has to wait')
+    elif a < 400:
+        return ('tiger can use this kid')
+    else:
+        return ('there\'s no kid that can be this heavy')
+
+a=float(input('Enter the bad kids weight in lbs: '))
+
+print(f'{readyfortiger(a)}')
