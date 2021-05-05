3d84e0a7014806185fef127b43624c8118a81cc4
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-05-05 15:37:17 -0700

    Update pow.py

--- pow.py
+++ pow.py
@@ -1,9 +1,12 @@
 def pow(a, b):
-    f=1
-    for i in range (0,b):
-        f=f*a    
-    return (f)
-
+    if b > 0 or b == 0:
+        f=1
+        for i in range (0,b):
+            f=f*a    
+        return (f)
+    else:
+        return (1.0/pow(a,-b))
+        
 a = float(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
 
