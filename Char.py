3960bd04217ad13f89461b754ef3c99e2775f77b
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-06-01 19:52:33 -0700

    Update Char.py

--- Char.py
+++ Char.py
@@ -1,4 +1,6 @@
 a = (input('Enter char : '))
 
 
-print(f'The char you entered is {a} is {(a)}')
+print(f'The char you entered is {(a)}')
+print(f'The unicode is', ord(a))
+print(f'The binary is ',bin(ord(a)))
