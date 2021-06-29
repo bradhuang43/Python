0951296e093fb9a70710390a39818dc28af7899e
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-06-28 19:10:10 -0700

    Create Celsius2Fahrenheit.py

---
+++ Celsius2Fahrenheit.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def c2f(a):
+    return (9/5*a+32)
+
+a = int(input('Enter the number: '))
+
+print(f'The equivalent fahrenheit is {c2f(a)}')
