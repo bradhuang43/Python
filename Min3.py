1f450f1285a6765ed5d28eecb0887a0138b36147
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-07-02 18:50:07 -0700

    Update Min3.py

--- Min3.py
+++ Min3.py
@@ -9,11 +9,10 @@ def min2(a, b):
 
 def min3(a, b, c):
     d = min2(a, b)
-    print(f'd is {d}')
     return min2(d, c)
    
-a = (input('Enter 1st number: '))
-b = (input('Enter 2nd number: '))
-c = (input('Enter 3rd number: '))
+a = float(input('Enter 1st number: '))
+b = float(input('Enter 2nd number: '))
+c = float(input('Enter 3rd number: '))
 
 print(f'min of all is {min3(a, b, c)}')
