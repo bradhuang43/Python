fe08a0f3190df5b804b554a186fd365e3854c969
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-07-02 18:53:37 -0700

    Create Min2.py

---
+++ Min2.py
@@ -0,0 +1,13 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def min2(a, b):
+    if a < b:
+        return a
+    else:
+        return b
+
+a = float(input('Enter 1st number: '))
+b = float(input('Enter 2nd number: '))
+
+print(f'min of this two is {min2(a, b)}')
