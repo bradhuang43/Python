a4ec4913e06c94db55f4cb6237d80fcb16b97b39
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-07-02 18:22:31 -0700

    Update and rename Min.py to Min3.py

--- Min3.py
+++ Min3.py
@@ -1,13 +1,19 @@
 
 # Online Python - IDE, Editor, Compiler, Interpreter
 
-def min(a, b):
+def min2(a, b):
     if a < b:
         return a
     else:
         return b
 
+def min3(a, b, c):
+    d = min2(a, b)
+    print(f'd is {d}')
+    return min2(d, c)
+   
 a = (input('Enter 1st number: '))
 b = (input('Enter 2nd number: '))
+c = (input('Enter 3rd number: '))
 
-print(f'min of {a} and {b} is {min(a, b)}')
+print(f'min of all is {min3(a, b, c)}')
