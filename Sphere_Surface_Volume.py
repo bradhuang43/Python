f87ed101c192e5d2a6a06847728d07b1416ec9e0
bradhuang43 <81834876+bradhuang43@users.noreply.github.com>
2021-08-14 19:35:39 -0700

    Create Sphere_Surface_Volume.py

---
+++ Sphere_Surface_Volume.py
@@ -0,0 +1,10 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+import math
+r = -1.0
+while r < 0:
+  r = float(input('Enter a positive radius: '))
+
+print(f'Surface of the sphere is {4*math.pi*r*r}')
+print(f'Volume of the sphere is {4.0/3.0*math.pi*r*r*r}')
